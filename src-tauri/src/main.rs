5397c031b4636693b87c33ea372bf029f78a286f
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-03-22 10:59:41 -0500

    worked on linux support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,10 @@
   windows_subsystem = "windows"
 )]
 
-use std::io::*;
+use std::{
+  io::*,
+  env
+};
 
 use std::path::{
   PathBuf,
@@ -14,6 +17,7 @@ use std::fs::{create_dir_all, File, OpenOptions};
 
 use chrono::prelude::*;
 
+use home::home_dir;
 use winreg::enums::*;
 use winreg::RegKey;
 
@@ -22,6 +26,19 @@ use tauri::AppHandle;
 fn get_log_path(app_handle: &AppHandle) -> PathBuf {
   let app_log_dir = app_handle.to_owned().path_resolver().app_log_dir().expect("Tried to resolve app log dir and failed.");
   return app_log_dir.join("steam-art-manager.log");
+}
+
+fn get_steam_root_dir() -> PathBuf {
+  let home_dir = home_dir().expect("Couldn't get user's home dir.");
+  let mut steam_dir = home_dir.clone();
+
+  if home_dir.join("/.var/app/com.valvesoftware.Steam/data/Steam").exists() {
+    steam_dir = steam_dir.join("/.var/app/com.valvesoftware.Steam/data/Steam");
+  } else {
+    steam_dir = steam_dir.join("/.steam/steam");
+  }
+
+  return steam_dir;
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -71,53 +88,124 @@ fn clean_out_log(app_handle: AppHandle) {
 }
 
 #[tauri::command]
-fn get_active_user() -> u32 {
-  let hkcu: RegKey = RegKey::predef(HKEY_CURRENT_USER);
+fn get_active_user(app_handle: AppHandle) -> u32 {
+  let platform = env::consts::OS;
 
-  let steam_active_process: RegKey = hkcu.open_subkey("SOFTWARE\\Valve\\Steam\\ActiveProcess").expect("Couldn't getActiveProcess from the registry");
-  let active_user_dword: u32 = steam_active_process.get_value("ActiveUser").expect("Couldn't get ActiveUser from the registry");
+  if platform == "windows" {
+    log_to_file(app_handle, "Checking registry for current user.".to_owned(), 0);
+    let hkcu: RegKey = RegKey::predef(HKEY_CURRENT_USER);
 
-  return active_user_dword;
+    let steam_active_process: RegKey = hkcu.open_subkey("SOFTWARE\\Valve\\Steam\\ActiveProcess").expect("Couldn't getActiveProcess from the registry");
+    let active_user_dword: u32 = steam_active_process.get_value("ActiveUser").expect("Couldn't get ActiveUser from the registry");
+
+    log_to_file(app_handle, format!("Got current_user: {}", active_user_dword), 0);
+
+    return active_user_dword;
+  } else if platform == "linux" {
+    log_to_file(app_handle, "Checking /config/loginusers.vdf for current user info.".to_owned(), 0);
+    
+    let steam_root = get_steam_root_dir();
+    let loginusers_vdf = steam_root.join("/config/loginusers.vdf");
+
+    //* How to get the id:
+    // find like starting with 7656119
+    // substring at that index till \"
+    // if last
+    //    convert to long
+    //    subtract 76561197960265728
+
+    // ex:
+    //    76561198884918228
+    //   -76561197960265728
+    // --------------------
+    //    00000000924652500
+    // aka 924652500
+
+    // log_to_file(app_handle, format!("Got current_user: {}", active_user_dword), 0);
+
+    return 0;
+  } else {
+    panic!("Steam Art Manager can only be run on linux or windows!");
+  }
 }
 
 #[tauri::command]
-fn get_steam_games() -> String {
-  let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-  let steam_apps_reg = hkcu.open_subkey("SOFTWARE\\Valve\\Steam\\Apps").expect("Couldn't Apps from the registry");
-
+fn get_steam_games(app_handle: AppHandle) -> String {
   let mut steam_apps: String = "".to_owned();
+  let platform = env::consts::OS;
 
-  for field in steam_apps_reg.enum_keys().map(|x| x.unwrap()) {
-    let mut app: String = "".to_owned();
-    app.push_str("\"appId\":");
-    app.push_str(&field);
-    app.push_str(",");
+  if platform == "windows" {
+    log_to_file(app_handle, "Checking registry for steam games.".to_owned(), 0);
 
-    let app_reg: RegKey = steam_apps_reg.open_subkey(field).expect("Couldn't get app from registry");
-    let mut app_name = "";
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let steam_apps_reg = hkcu.open_subkey("SOFTWARE\\Valve\\Steam\\Apps").expect("Couldn't Apps from the registry");
 
-    let app_name_reg: Result<String> = app_reg.get_value("Name");
+    for field in steam_apps_reg.enum_keys().map(|x| x.unwrap()) {
+      let mut app: String = "".to_owned();
+      app.push_str("\"appId\":");
+      app.push_str(&field);
+      app.push_str(",");
 
-    if app_name_reg.is_ok() {
-      app_name = app_name_reg.as_ref().unwrap();
+      let app_reg: RegKey = steam_apps_reg.open_subkey(field).expect("Couldn't get app from registry");
+      let mut app_name = "";
+
+      let app_name_reg: Result<String> = app_reg.get_value("Name");
+
+      if app_name_reg.is_ok() {
+        app_name = app_name_reg.as_ref().unwrap();
+      }
+      
+      app.push_str("\"name\":\"");
+      app.push_str(app_name);
+      app.push_str("\",");
+      let mut updated_app = "".to_owned();
+      updated_app.push_str("{");
+      updated_app.push_str(&app[..(app.len() - 1)]);
+      updated_app.push_str("},");
+
+      steam_apps.push_str(&updated_app);
     }
-    
-    app.push_str("\"name\":\"");
-    app.push_str(app_name);
-    app.push_str("\",");
-    let mut updated_app = "".to_owned();
-    updated_app.push_str("{");
-    updated_app.push_str(&app[..(app.len() - 1)]);
-    updated_app.push_str("},");
+  } else if platform == "linux" {
+    log_to_file(app_handle, "Checking registry.vdf for steam games.".to_owned(), 0);
 
-    steam_apps.push_str(&updated_app);
+    let steam_root = get_steam_root_dir();
+
+    // let steam_apps_reg = hkcu.open_subkey("SOFTWARE\\Valve\\Steam\\Apps").expect("Couldn't Apps from the registry");
+
+    // for field in steam_apps_reg.enum_keys().map(|x| x.unwrap()) {
+    //   let mut app: String = "".to_owned();
+    //   app.push_str("\"appId\":");
+    //   app.push_str(&field);
+    //   app.push_str(",");
+
+    //   let app_reg: RegKey = steam_apps_reg.open_subkey(field).expect("Couldn't get app from registry");
+    //   let mut app_name = "";
+
+    //   let app_name_reg: Result<String> = app_reg.get_value("Name");
+
+    //   if app_name_reg.is_ok() {
+    //     app_name = app_name_reg.as_ref().unwrap();
+    //   }
+      
+    //   app.push_str("\"name\":\"");
+    //   app.push_str(app_name);
+    //   app.push_str("\",");
+    //   let mut updated_app = "".to_owned();
+    //   updated_app.push_str("{");
+    //   updated_app.push_str(&app[..(app.len() - 1)]);
+    //   updated_app.push_str("},");
+
+    //   steam_apps.push_str(&updated_app);
+    // }
+  } else {
+    panic!("Steam Art Manager can only be run on linux or windows!");
   }
 
   let mut updated_apps = "".to_owned();
   updated_apps.push_str(&"[");
   updated_apps.push_str(&steam_apps[..(steam_apps.len() - 1)]);
   updated_apps.push_str(&"]");
-  
+    
   return updated_apps;
 }
 
