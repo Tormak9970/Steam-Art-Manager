0a590ae57ffd88a4bceb10789c08e1aac49aa720
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-04-01 22:20:56 -0500

    build: rem forced target_os so app should compile

--- src-tauri/src/steam.rs
+++ src-tauri/src/steam.rs
@@ -1,22 +1,18 @@
 
 use crate::logger;
 
-use std::{ io::*, fs };
+use std::fs;
 use std::path::{ PathBuf, Path };
 use std::u32;
 
 #[cfg(target_os = "windows")]
-use winreg::enums::*;
-
-#[cfg(target_os = "windows")]
-use winreg::RegKey;
+use winreg::{ enums::*, RegKey };
 
 use tauri::AppHandle;
 
-#[cfg(target_os = "ubuntu-20.04")]
+#[cfg(target_os = "linux")]
 use home::home_dir;
-
-#[cfg(target_os = "ubuntu-20.04")]
+#[cfg(target_os = "linux")]
 use crate::vdf_structs;
 
 #[cfg(target_os = "windows")]
@@ -29,7 +25,7 @@ pub fn get_steam_root_dir() -> PathBuf {
   return Path::new(&(steam_install_path.replace("\\", "/"))).to_path_buf();
 }
 
-#[cfg(target_os = "ubuntu-20.04")]
+#[cfg(target_os = "linux")]
 pub fn get_steam_root_dir() -> PathBuf {
   let pc_home_dir = home_dir().expect("Couldn't get user's home dir.");
   let mut steam_dir = home_dir.clone();
@@ -91,7 +87,7 @@ pub fn get_active_user(app_handle: AppHandle) -> u32 {
 }
 
 #[tauri::command]
-#[cfg(target_os = "ubuntu-20.04")]
+#[cfg(target_os = "linux")]
 pub fn get_active_user(app_handle: AppHandle) -> u32 {
   logger::log_to_file(app_handle.to_owned(), "Checking config/loginusers.vdf for current user info.", 0);
     
@@ -116,90 +112,91 @@ pub fn get_active_user(app_handle: AppHandle) -> u32 {
   return 0;
 }
 
-#[tauri::command]
-#[cfg(target_os = "windows")]
-pub fn get_steam_apps(app_handle: AppHandle) -> String {
-  let mut steam_apps: String = "".to_owned();
+// ! This isn't needed anymore but leaving it because in the event something changes its still quite useful.
+// #[tauri::command]
+// #[cfg(target_os = "windows")]
+// pub fn get_steam_apps(app_handle: AppHandle) -> String {
+//   let mut steam_apps: String = "".to_owned();
 
-  logger::log_to_file(app_handle.to_owned(), "Checking registry for steam games.", 0);
+//   logger::log_to_file(app_handle.to_owned(), "Checking registry for steam games.", 0);
 
-  let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-  let steam_apps_reg = hkcu.open_subkey("SOFTWARE\\Valve\\Steam\\Apps").expect("Couldn't Apps from the registry");
+//   let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+//   let steam_apps_reg = hkcu.open_subkey("SOFTWARE\\Valve\\Steam\\Apps").expect("Couldn't Apps from the registry");
 
-  for field in steam_apps_reg.enum_keys().map(|x| x.unwrap()) {
-    let mut app: String = "".to_owned();
-    app.push_str("\"appId\":");
-    app.push_str(&field);
-    app.push_str(",");
+//   for field in steam_apps_reg.enum_keys().map(|x| x.unwrap()) {
+//     let mut app: String = "".to_owned();
+//     app.push_str("\"appId\":");
+//     app.push_str(&field);
+//     app.push_str(",");
 
-    let app_reg: RegKey = steam_apps_reg.open_subkey(field).expect("Couldn't get app from registry");
-    let mut app_name = "";
+//     let app_reg: RegKey = steam_apps_reg.open_subkey(field).expect("Couldn't get app from registry");
+//     let mut app_name = "";
 
-    let app_name_reg: Result<String> = app_reg.get_value("Name");
+//     let app_name_reg: Result<String> = app_reg.get_value("Name");
 
-    if app_name_reg.is_ok() {
-      app_name = app_name_reg.as_ref().unwrap();
-    }
+//     if app_name_reg.is_ok() {
+//       app_name = app_name_reg.as_ref().unwrap();
+//     }
     
-    app.push_str("\"name\":\"");
-    app.push_str(app_name);
-    app.push_str("\",");
-    let mut updated_app = "".to_owned();
-    updated_app.push_str("{");
-    updated_app.push_str(&app[..(app.len() - 1)]);
-    updated_app.push_str("},");
+//     app.push_str("\"name\":\"");
+//     app.push_str(app_name);
+//     app.push_str("\",");
+//     let mut updated_app = "".to_owned();
+//     updated_app.push_str("{");
+//     updated_app.push_str(&app[..(app.len() - 1)]);
+//     updated_app.push_str("},");
 
-    steam_apps.push_str(&updated_app);
-  }
+//     steam_apps.push_str(&updated_app);
+//   }
 
-  let mut updated_apps = "".to_owned();
-  updated_apps.push_str(&"[");
-  updated_apps.push_str(&steam_apps[..(steam_apps.len() - 1)]);
-  updated_apps.push_str(&"]");
+//   let mut updated_apps = "".to_owned();
+//   updated_apps.push_str(&"[");
+//   updated_apps.push_str(&steam_apps[..(steam_apps.len() - 1)]);
+//   updated_apps.push_str(&"]");
     
-  return updated_apps;
-}
+//   return updated_apps;
+// }
 
-#[tauri::command]
-#[cfg(target_os = "ubuntu-20.04")]
-pub fn get_steam_apps(app_handle: AppHandle) -> String {
-  let mut steam_apps: String = "".to_owned();
+// #[tauri::command]
+// #[cfg(target_os = "linux")]
+// pub fn get_steam_apps(app_handle: AppHandle) -> String {
+//   let mut steam_apps: String = "".to_owned();
 
-  logger::log_to_file(app_handle.to_owned(), "Checking registry.vdf for steam games.", 0);
+//   logger::log_to_file(app_handle.to_owned(), "Checking registry.vdf for steam games.", 0);
 
-  let steam_root = get_steam_root_dir();
-  let registry_vdf = steam_root.parent().expect("Parent should have existed").join("registry.vdf");
-  let contents = fs::read_to_string(registry_vdf).unwrap();
+//   let steam_root = get_steam_root_dir();
+//   let registry_vdf = steam_root.parent().expect("Parent should have existed").join("registry.vdf");
+//   let contents = fs::read_to_string(registry_vdf).unwrap();
 
-  let steam_apps_res = vdf_serde::from_str::<vdf_structs::Registry>(&contents).unwrap().HKCU.Software.Valve.Steam.apps;
+//   let steam_apps_res = vdf_serde::from_str::<vdf_structs::Registry>(&contents).unwrap().HKCU.Software.Valve.Steam.apps;
 
-  for (key, value) in steam_apps_res.into_iter() {
-    let mut app: String = "".to_owned();
-    app.push_str("\"appId\":");
-    app.push_str(&key);
-    app.push_str(",");
+//   for (key, value) in steam_apps_res.into_iter() {
+//     let mut app: String = "".to_owned();
+//     app.push_str("\"appId\":");
+//     app.push_str(&key);
+//     app.push_str(",");
 
-    let mut app_name = "";
+//     let mut app_name = "";
 
-    if value.contains_key("name") {
-      app_name = value.get("name").unwrap().as_ref();
-    }
+//     if value.contains_key("name") {
+//       app_name = value.get("name").unwrap().as_ref();
+//     }
     
-    app.push_str("\"name\":\"");
-    app.push_str(app_name);
-    app.push_str("\",");
-    let mut updated_app = "".to_owned();
-    updated_app.push_str("{");
-    updated_app.push_str(&app[..(app.len() - 1)]);
-    updated_app.push_str("},");
+//     app.push_str("\"name\":\"");
+//     app.push_str(app_name);
+//     app.push_str("\",");
+//     let mut updated_app = "".to_owned();
+//     updated_app.push_str("{");
+//     updated_app.push_str(&app[..(app.len() - 1)]);
+//     updated_app.push_str("},");
 
-    steam_apps.push_str(&updated_app);
-  }
+//     steam_apps.push_str(&updated_app);
+//   }
 
-  let mut updated_apps = "".to_owned();
-  updated_apps.push_str(&"[");
-  updated_apps.push_str(&steam_apps[..(steam_apps.len() - 1)]);
-  updated_apps.push_str(&"]");
+//   let mut updated_apps = "".to_owned();
+//   updated_apps.push_str(&"[");
+//   updated_apps.push_str(&steam_apps[..(steam_apps.len() - 1)]);
+//   updated_apps.push_str(&"]");
     
-  return updated_apps;
-}
+//   return updated_apps;
+// }
