0d81163309a2651203e2a1b604df85ea3b05532c
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-04-13 18:05:05 -0500

    chore: writer complete

--- src-tauri/src/shortcuts_vdf_parser.rs
+++ src-tauri/src/shortcuts_vdf_parser.rs
@@ -66,6 +66,54 @@ fn read_entry_field(reader: &mut Reader, field_type: u8) -> Value {
   }
 }
 
-pub fn write_shortcuts_vdf(path: &PathBuf) {
+pub fn write_shortcuts_vdf(path: &PathBuf, data: Value) {
   
+}
+
+fn write(reader: &mut Reader) -> Value {
+  reader.seek(1, 0);
+
+  let fake_header = reader.read_string(None);
+
+  if fake_header != "shortcuts".to_owned() {
+    panic!("Invalid Shortcuts File! File started with {} instead of \"shortcuts\"", fake_header);
+  }
+
+  return read_entry_map(reader);
+}
+
+fn write_entry_map(reader: &mut Reader) -> Value {
+  let mut props = Map::new();
+
+  let mut field_type = reader.read_uint8(true);
+
+  while field_type != 0x08 {
+    let key = reader.read_string(None);
+    let value = read_entry_field(reader, field_type);
+
+    props.insert(key, value);
+
+    field_type = reader.read_uint8(true);
+  }
+
+  return Value::Object(props);
+}
+
+fn write_entry_field(reader: &mut Reader, field_type: u8) -> Value {
+  match field_type {
+    0x00 => { //? map
+      return read_entry_map(reader);
+    },
+    0x01 => { //? string
+      let value = reader.read_string(None);
+      return Value::String(value);
+    },
+    0x02 => { //? number
+      let value = reader.read_uint32(true);
+      return Value::Number(value.into());
+    },
+    _ => {
+      panic!("Unexpected field type {}!", field_type);
+    }
+  }
 }
--- src-tauri/src/writer.rs
+++ src-tauri/src/writer.rs
@@ -1,27 +1,27 @@
 trait HasByteConvert {
-  fn to_le_bytes(bytes: &mut [u8], data: Self, offset: usize) -> u8;
-  fn to_be_bytes(bytes: &mut [u8], data: Self, offset: usize) -> u8;
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: Self, offset: usize) -> u8;
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: Self, offset: usize) -> u8;
 }
 
 impl HasByteConvert for u8 {
-  fn to_le_bytes(bytes: &mut [u8], data: u8, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: u8, offset: usize) -> u8 {
     bytes[offset] = data;
     return 1;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: u8, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: u8, offset: usize) -> u8 {
     bytes[offset] = data;
     return 1;
   }
 }
 
 impl HasByteConvert for u16 {
-  fn to_le_bytes(bytes: &mut [u8], data: u16, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: u16, offset: usize) -> u8 {
     let u8_arr = data.to_le_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
     return 2;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: u16, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: u16, offset: usize) -> u8 {
     let u8_arr = data.to_be_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -30,7 +30,7 @@ impl HasByteConvert for u16 {
 }
 
 impl HasByteConvert for u32 {
-  fn to_le_bytes(bytes: &mut [u8], data: u32, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: u32, offset: usize) -> u8 {
     let u8_arr = data.to_le_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -38,7 +38,7 @@ impl HasByteConvert for u32 {
     bytes[offset+3] = u8_arr[3];
     return 4;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: u32, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: u32, offset: usize) -> u8 {
     let u8_arr = data.to_be_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -49,7 +49,7 @@ impl HasByteConvert for u32 {
 }
 
 impl HasByteConvert for u64 {
-  fn to_le_bytes(bytes: &mut [u8], data: u64, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: u64, offset: usize) -> u8 {
     let u8_arr = data.to_le_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -61,7 +61,7 @@ impl HasByteConvert for u64 {
     bytes[offset+7] = u8_arr[7];
     return 8;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: u64, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: u64, offset: usize) -> u8 {
     let u8_arr = data.to_be_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -76,24 +76,24 @@ impl HasByteConvert for u64 {
 }
 
 impl HasByteConvert for i8 {
-  fn to_le_bytes(bytes: &mut [u8], data: i8, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: i8, offset: usize) -> u8 {
     bytes[offset] = data.to_le_bytes()[0];
     return 1;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: i8, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: i8, offset: usize) -> u8 {
     bytes[offset] = data.to_be_bytes()[0];
     return 1;
   }
 }
 
 impl HasByteConvert for i16 {
-  fn to_le_bytes(bytes: &mut [u8], data: i16, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: i16, offset: usize) -> u8 {
     let u8_arr = data.to_le_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
     return 2;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: i16, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: i16, offset: usize) -> u8 {
     let u8_arr = data.to_be_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -102,7 +102,7 @@ impl HasByteConvert for i16 {
 }
 
 impl HasByteConvert for i32 {
-  fn to_le_bytes(bytes: &mut [u8], data: i32, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: i32, offset: usize) -> u8 {
     let u8_arr = data.to_le_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -110,7 +110,7 @@ impl HasByteConvert for i32 {
     bytes[offset+3] = u8_arr[3];
     return 4;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: i32, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: i32, offset: usize) -> u8 {
     let u8_arr = data.to_be_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -121,7 +121,7 @@ impl HasByteConvert for i32 {
 }
 
 impl HasByteConvert for i64 {
-  fn to_le_bytes(bytes: &mut [u8], data: i64, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: i64, offset: usize) -> u8 {
     let u8_arr = data.to_le_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -133,7 +133,7 @@ impl HasByteConvert for i64 {
     bytes[offset+7] = u8_arr[7];
     return 8;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: i64, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: i64, offset: usize) -> u8 {
     let u8_arr = data.to_be_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -148,7 +148,7 @@ impl HasByteConvert for i64 {
 }
 
 impl HasByteConvert for f32 {
-  fn to_le_bytes(bytes: &mut [u8], data: f32, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: f32, offset: usize) -> u8 {
     let u8_arr = data.to_le_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -156,7 +156,7 @@ impl HasByteConvert for f32 {
     bytes[offset+3] = u8_arr[3];
     return 4;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: f32, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: f32, offset: usize) -> u8 {
     let u8_arr = data.to_be_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -167,7 +167,7 @@ impl HasByteConvert for f32 {
 }
 
 impl HasByteConvert for f64 {
-  fn to_le_bytes(bytes: &mut [u8], data: f64, offset: usize) -> u8 {
+  fn to_le_bytes(bytes: &mut Vec<u8>, data: f64, offset: usize) -> u8 {
     let u8_arr = data.to_le_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -179,7 +179,7 @@ impl HasByteConvert for f64 {
     bytes[offset+7] = u8_arr[7];
     return 8;
   }
-  fn to_be_bytes(bytes: &mut [u8], data: f64, offset: usize) -> u8 {
+  fn to_be_bytes(bytes: &mut Vec<u8>, data: f64, offset: usize) -> u8 {
     let u8_arr = data.to_be_bytes();
     bytes[offset] = u8_arr[0];
     bytes[offset+1] = u8_arr[1];
@@ -194,38 +194,16 @@ impl HasByteConvert for f64 {
 }
 
 pub struct Writer<'a> {
-  data: &'a mut [u8],
+  data: &'a mut Vec<u8>,
   offset: usize,
-  length: usize,
 }
 
 #[allow(dead_code)]
 impl Writer<'_> {
-  pub fn get_data(&self) -> &[u8] { return self.data; }
+  pub fn get_data(&self) -> &[u8] { return &self.data[..]; }
   pub fn get_offset(&self) -> usize { return self.offset; }
-  pub fn get_length(&self) -> usize { return self.length; }
 
-  pub fn new(buf: &mut [u8]) -> Writer { return Writer { data: buf, offset: 0, length: buf.len() }; }
-
-  fn expand_capacity(&mut self) {
-    let mut new_data: Vec<u8> = vec![0; self.length.try_into().unwrap()];
-    let mut u8_vec: Vec<u8> = self.data.to_vec();
-    let mut_u8_vec: &mut Vec<u8> = u8_vec.as_mut();
-    
-    mut_u8_vec.append(new_data.as_mut());
-    
-    self.length = self.length * 2;
-
-    self.data = mut_u8_vec[..].as_mut();
-  }
-
-  pub fn trim(&mut self) {
-    let data = &self.data[0..self.offset];
-    let owned_data = data.to_owned();
-
-    self.length = self.offset;
-    self.data = owned_data[..].as_mut();
-  }
+  pub fn new(buf: &mut Vec<u8>) -> Writer { return Writer { data: buf, offset: 0 }; }
 
   pub fn seek(&mut self, offset: usize, position: u8) {
     if position == 0 {
@@ -233,16 +211,11 @@ impl Writer<'_> {
     } else if position == 1 {
       self.offset += offset;
     } else {
-      self.offset = (self.length as usize) - offset;
+      self.offset = (self.data.len() as usize) - offset;
     }
   }
   
-  pub fn remaining(&mut self) -> usize { return self.length - (self.offset + 1); }
-
-  fn write_i<T: HasByteConvert>(&mut self, data: T, length: u8, endianness: bool) -> u8 {
-    if self.remaining() <= length.into() {
-      self.expand_capacity();
-    }
+  fn write_i<T: HasByteConvert>(&mut self, data: T, endianness: bool) -> u8 {
     if endianness {
       return T::to_le_bytes(self.data, data, self.offset);
     } else {
@@ -251,54 +224,54 @@ impl Writer<'_> {
   }
   
   pub fn write_uint8(&mut self, data: u8, endianness: bool) -> u8 {
-    let res = self.write_i::<u8>(data, 1, endianness);
+    let res = self.write_i::<u8>(data, endianness);
     self.offset += 1;
     return res;
   }
   pub fn write_uint16(&mut self, data: u16, endianness: bool) -> u8 {
-    let res = self.write_i::<u16>(data, 2, endianness);
+    let res = self.write_i::<u16>(data, endianness);
     self.offset += 2;
     return res;
   }
   pub fn write_uint32(&mut self, data: u32, endianness: bool) -> u8 {
-    let res = self.write_i::<u32>(data, 4, endianness);
+    let res = self.write_i::<u32>(data, endianness);
     self.offset += 4;
     return res;
   }
   pub fn write_uint64(&mut self, data: u64, endianness: bool) -> u8 {
-    let res = self.write_i::<u64>(data, 8, endianness);
+    let res = self.write_i::<u64>(data, endianness);
     self.offset += 8;
     return res;
   }
   
   pub fn write_int8(&mut self, data: i8, endianness: bool) -> u8 {
-    let res = self.write_i::<i8>(data, 1, endianness);
+    let res = self.write_i::<i8>(data, endianness);
     self.offset += 1;
     return res;
   }
   pub fn write_int16(&mut self, data: i16, endianness: bool) -> u8 {
-    let res = self.write_i::<i16>(data, 2, endianness);
+    let res = self.write_i::<i16>(data, endianness);
     self.offset += 2;
     return res;
   }
   pub fn write_int32(&mut self, data: i32, endianness: bool) -> u8 {
-    let res = self.write_i::<i32>(data, 4, endianness);
+    let res = self.write_i::<i32>(data, endianness);
     self.offset += 4;
     return res;
   }
   pub fn write_int64(&mut self, data: i64, endianness: bool) -> u8 {
-    let res = self.write_i::<i64>(data, 8, endianness);
+    let res = self.write_i::<i64>(data, endianness);
     self.offset += 8;
     return res;
   }
   
   pub fn write_float32(&mut self, data: f32, endianness: bool) -> u8 {
-    let res = self.write_i::<f32>(data, 4, endianness);
+    let res = self.write_i::<f32>(data, endianness);
     self.offset += 4;
     return res;
   }
   pub fn write_float64(&mut self, data: f64, endianness: bool) -> u8 {
-    let res = self.write_i::<f64>(data, 8, endianness);
+    let res = self.write_i::<f64>(data, endianness);
     self.offset += 8;
     return res;
   }
