0cdff3dbd75755bfcfe9c03ab37a51cd9aba4008
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-05-27 13:53:23 -0500

    feat: added uninstall script

--- build-resources/linux-uinstaller.sh
+++ build-resources/linux-uinstaller.sh
@@ -12,18 +12,21 @@ if zenity --question --title="Warning!" --text="You are about to uninstall SARM 
   rm -rf "$HOME/.sarm"
 
   # Remove Desktop icons
-  rm -rf "$HOME"/Desktop/SARM.desktop
-  rm -rf "$HOME"/Desktop/UninstallSARM.desktop
+  rm -rf "$HOME/Desktop/SARM.desktop"
+  rm -rf "$HOME/Desktop/UninstallSARM.desktop"
 
   # Remove Start Menu shortcuts
-  rm -rf "$HOME"/.local/share/applications/SARM.desktop
-  rm -rf "$HOME"/.local/share/applications/UninstallSARM.desktop
+  rm -rf "$HOME/.local/share/applications/SARM.desktop"
+  rm -rf "$HOME/.local/share/applications/UninstallSARM.desktop"
 
   # Remove cache directory
+  rm -rf "$HOME/.cache/dev.tormak.steam-art-manager"
 
   # Remove config directory
+  rm -rf "$HOME/.config/dev.tormak.steam-art-manager"
 
   # Remove Tauri Files
+  rm -rf "$HOME/.local/share/dev.tormak.steam-art-manager"
 
   update-desktop-database $HOME/.local/share/applications
 
