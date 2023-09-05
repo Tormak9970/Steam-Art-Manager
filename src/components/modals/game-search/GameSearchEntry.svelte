1056b55e49c022a7891002f2fa30da7cb22aaad3
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-09-05 18:35:20 -0400

    chore: currently selected game in search is now indicated

--- src/components/modals/game-search/GameSearchEntry.svelte
+++ src/components/modals/game-search/GameSearchEntry.svelte
@@ -9,11 +9,7 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="game-search-entry" on:click={() => onSelect(game)}>
   <svg xmlns="http://www.w3.org/2000/svg" height="16px" width="16px" viewBox="0 0 100 100" style="height: 16px; width: 16px;">
-    <g fill-rule="evenodd" fill="black" stroke="black" stroke-width="3">
-      <circle cx="50" cy="50" r="44" />
-      <circle cx="50" cy="50" r="28" />
-    </g>
-  
+    <circle cx="50" cy="50" r="38" fill={isSelected ? "var(--font-color)" : "transparent"} style="transition: fill 0.3s ease-in-out;" stroke="var(--font-color)" stroke-width="4" />
   </svg>
   <div class="name">{game.name}</div>
 </div>
