277bf745b5220a1ca5c23eac33c5ded35700d40e
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-10-24 09:20:06 -0400

    chore: tweaked view toggle icon

--- src/components/core/games/Games.svelte
+++ src/components/core/games/Games.svelte
@@ -152,16 +152,17 @@
         <IconToggle onChange={onViewTypeChange} leftTooltip="Grid View" rightTooltip="List View" bind:value={$renderGamesInList}>
           <span slot="left">
             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-              <rect x="0" y="10" width="220" height="220" rx="50" ry="50" />
-              <rect x="290" y="10" width="220" height="220" rx="50" ry="50" />
-              <rect x="0" y="290" width="220" height="220" rx="50" ry="50" />
-              <rect x="290" y="290" width="220" height="220" rx="50" ry="50" />
+              <rect x="6" y="6" width="220" height="220" rx="25" ry="25" />
+              <rect x="286" y="6" width="220" height="220" rx="25" ry="25" />
+              <rect x="6" y="286" width="220" height="220" rx="25" ry="25" />
+              <rect x="286" y="286" width="220" height="220" rx="25" ry="25" />
             </svg>
           </span>
           <span slot="right">
             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-              <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-              <path d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z">
+              <rect x="6" y="46" width="500" height="100" rx="25" ry="25" />
+              <rect x="6" y="206" width="500" height="100" rx="25" ry="25" />
+              <rect x="6" y="366" width="500" height="100" rx="25" ry="25" />
             </svg>
           </span>
         </IconToggle>
