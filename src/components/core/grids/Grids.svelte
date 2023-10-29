030c0860c7bf98c3c3ac1e09b629070bdc6f2fb3
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-10-29 16:12:09 -0400

    fix: tooltip text is now shorter where possible

--- src/components/core/grids/Grids.svelte
+++ src/components/core/grids/Grids.svelte
@@ -295,7 +295,7 @@
           </IconButton>
           <Spacer orientation="HORIZONTAL" />
 
-          <IconButton label="Upload Your Own Art!" onClick={prompUserForArt} width="auto" disabled={!$selectedGameAppId}>
+          <IconButton label="Upload Local Art" onClick={prompUserForArt} width="auto" disabled={!$selectedGameAppId}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="height: 12px; width: 12px;">
               <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
               <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM385 231c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-71-71V376c0 13.3-10.7 24-24 24s-24-10.7-24-24V193.9l-71 71c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9L239 119c9.4-9.4 24.6-9.4 33.9 0L385 231z"/>
