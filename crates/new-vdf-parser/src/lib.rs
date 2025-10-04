e761e8566d453cc190567d6c81983ed52f6f5873
Travis Lane <63308171+Tormak9970@users.noreply.github.com>
2025-10-04 19:58:22 -0400

    chore: refactor for package release

--- crates/new-vdf-parser/src/lib.rs
+++ crates/new-vdf-parser/src/lib.rs
@@ -3,7 +3,6 @@ pub mod writer;
 pub mod appinfo_vdf_parser;
 pub mod shortcuts_vdf_parser;
 mod vdf_reader;
-mod vdf_structs;
 
 pub use appinfo_vdf_parser::open_appinfo_vdf;
 pub use shortcuts_vdf_parser::{open_shortcuts_vdf, write_shortcuts_vdf};
